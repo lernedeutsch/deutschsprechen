66d2d36600a90f3c942345827c1378956bc8c4d4
deutsch.lernen <moni.s.w@o2.pl>
2026-09-02 17:46:42 +0200

    Add Nele module for German language teaching

--- nele/nele.js
+++ nele/nele.js
@@ -1,1 +1,16 @@
+/* ========================================
+   NELE
+   Główny moduł nauczycielki języka niemieckiego
+======================================== */
 
+const Nele = {
+    name: "Nele",
+    language: "de-DE",
+    level: "A1",
+
+    init() {
+        console.log("Nele ist bereit.");
+    }
+};
+
+Nele.init();
