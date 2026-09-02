0ca28afbbc6541c9537bb7ea640a6ccc9793bd41
deutsch.lernen <moni.s.w@o2.pl>
2026-09-02 17:55:42 +0200

    Update nele.js

--- nele/nele.js
+++ nele/nele.js
@@ -1,16 +1,1 @@
-/* ========================================
-   NELE
-   Główny moduł nauczycielki języka niemieckiego
-======================================== */
 
-const Nele = {
-    name: "Nele",
-    language: "de-DE",
-    level: "A1",
-
-    init() {
-        console.log("Nele ist bereit.");
-    }
-};
-
-Nele.init();
