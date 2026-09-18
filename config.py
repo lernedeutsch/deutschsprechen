61bfdb062fa207e68ea171156d2449d2d426229d
deutsch.lernen <moni.s.w@o2.pl>
2026-09-18 16:57:34 +0200

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-import os
-class Config:
-    SECRET_KEY=os.getenv('SECRET_KEY','dev-secret')
-    DATABASE_URL=os.getenv('DATABASE_URL','sqlite:///nele.db')
-    CORS_ORIGINS=[x.strip() for x in os.getenv('CORS_ORIGINS','http://127.0.0.1:5500,http://localhost:5500,https://lernedeutsch.github.io').split(',') if x.strip()]
