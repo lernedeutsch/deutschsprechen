8ac3d772306ff7404aaf6ab88a7ced8f5f51264e
deutsch.lernen <moni.s.w@o2.pl>
2026-09-22 19:08:26 +0200

    Test desktop tablet and phone layouts

--- playwright.config.js
+++ playwright.config.js
@@ -7,9 +7,34 @@ module.exports = defineConfig({
   use: {
     baseURL: "http://127.0.0.1:4173",
     trace: "retain-on-failure",
-    screenshot: "only-on-failure",
-    ...devices["Desktop Chrome"]
+    screenshot: "only-on-failure"
   },
+  projects: [
+    {
+      name: "Desktop Chrome",
+      use: {
+        ...devices["Desktop Chrome"]
+      }
+    },
+    {
+      name: "Android Tablet",
+      use: {
+        ...devices["Desktop Chrome"],
+        viewport: { width: 800, height: 1280 },
+        isMobile: true,
+        hasTouch: true
+      }
+    },
+    {
+      name: "Android Phone",
+      use: {
+        ...devices["Desktop Chrome"],
+        viewport: { width: 390, height: 844 },
+        isMobile: true,
+        hasTouch: true
+      }
+    }
+  ],
   webServer: {
     command: "python3 -m http.server 4173 --bind 127.0.0.1",
     url: "http://127.0.0.1:4173",
